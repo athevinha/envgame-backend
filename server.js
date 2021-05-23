636f8ea38a0c18100cb926b9ce7347ad04552e39
pvinhp <nguyenthevinh1002@gmail.com>
2021-05-23 14:13:03 +0700

    ok ok

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const db = require("./model");
-const { games } = require("./model");
-const game = db.games;
+const games = db.games;
 app.use(cors(corsOptions));
 app.use(express.json());
 
@@ -23,17 +22,12 @@ db.mongoose
 var corsOptions = {
   origin: "http://localhost:8081",
 };
-
-app.get("/api/create", async (req, res) => {
-  let gamea = new game({
-    title: "egweg",
-    description: "qweg",
-    url: "QE",
-    publish: true,
-  });
+app.post("/api/games/create", async (req, res) => {
+  let gamea = new games(req.body);
+  console.log(gamea);
   try {
     await gamea.save();
-    res.send("hahah");
+    res.send("Create game successfully !");
   } catch (e) {
     console.log(e);
   }
@@ -46,6 +40,36 @@ app.get("/api/games/read", async (req, res) => {
     res.send(database);
   });
 });
+app.put("/api/games/update/:id", async (req, res) => {
+  let id = req.params.id;
+  let updateGame = req.body;
+  try {
+    await games.findById(id, (err, game) => {
+      game.title = updateGame.newTitle == "" ? game.title : updateGame.newTitle;
+      game.description =
+        updateGame.newDescription == ""
+          ? game.description
+          : updateGame.newDescription;
+      game.url = updateGame.newUrl == "" ? game.url : updateGame.newUrl;
+      game.save();
+      res.send(game);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.delete("/api/games/delete/:id", async (req, resp) => {
+  let id = req.params.id;
+  await games.findOneAndDelete(id, (err, res) => {
+    try {
+      resp.send("delete ok");
+    } catch (err) {
+      console.log(err);
+      resp.send(err);
+    }
+  });
+});
+
 app.get("/api", (req, res) => {});
 
 // set port, listen for requests
