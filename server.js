2bb75c9f87f5ee28ee414e20171822960d68f396
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-08-30 13:18:39 +0700

    change url server

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ db.mongoose
 var corsOptions = {
   origin: "http://localhost:8081",
 };
-app.post("/api/games/create", async (req, res) => {
+app.post("/api/envgame/conchuot2945@/2945/games/create", async (req, res) => {
   let gamea = new games(req.body);
   try {
     await gamea.save();
@@ -43,7 +43,7 @@ app.post("/api/games/create", async (req, res) => {
     console.log(e);
   }
 });
-app.get("/api/games/read", async (req, res) => {
+app.get("/api/envgame/conchuot2945@/2945/games/read", async (req, res) => {
   games.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -51,71 +51,81 @@ app.get("/api/games/read", async (req, res) => {
     res.send(database);
   });
 });
-app.put("/api/games/update/:id", async (req, res) => {
-  let id = req.params.id;
-  let updateGame = req.body;
-  try {
-    await games.findById(id, (err, game) => {
-      //* title *
-      game.title = updateGame.newTitle == "" ? game.title : updateGame.newTitle;
-      //* description *
-      game.description =
-        updateGame.newDescription == ""
-          ? game.description
-          : updateGame.newDescription;
-      //* url *
-      game.url = updateGame.newUrl == "" ? game.url : updateGame.newUrl;
-      //* iframe *
-      game.iframe =
-        updateGame.newIframe == "" ? game.iframe : updateGame.newIframe;
-      //* love game *
-      game.love_game =
-        updateGame.newLove_game == null
-          ? game.love_game
-          : updateGame.newLove_game;
-      //* how2play *
-      game.how2play =
-        updateGame.newHow2play == "" ? game.how2play : updateGame.newHow2play;
-      //* Mobile game *
-      game.mobile_game =
-        updateGame.newNobile_game == null
-          ? game.mobile_game
-          : updateGame.newMobile_game;
-      console.log(game);
-      game.save();
-      res.send(game);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.delete("/api/games/delete/:id", async (req, res) => {
-  let _id = req.params.id;
-  games.findByIdAndDelete(_id, (err, txt) => {
+app.put(
+  "/api/envgame/conchuot2945@/2945/games/update/:id",
+  async (req, res) => {
+    let id = req.params.id;
+    let updateGame = req.body;
     try {
-      res.send("Xóa thành công _ID: " + _id);
+      await games.findById(id, (err, game) => {
+        //* title *
+        game.title =
+          updateGame.newTitle == "" ? game.title : updateGame.newTitle;
+        //* description *
+        game.description =
+          updateGame.newDescription == ""
+            ? game.description
+            : updateGame.newDescription;
+        //* url *
+        game.url = updateGame.newUrl == "" ? game.url : updateGame.newUrl;
+        //* iframe *
+        game.iframe =
+          updateGame.newIframe == "" ? game.iframe : updateGame.newIframe;
+        //* love game *
+        game.love_game =
+          updateGame.newLove_game == null
+            ? game.love_game
+            : updateGame.newLove_game;
+        //* how2play *
+        game.how2play =
+          updateGame.newHow2play == "" ? game.how2play : updateGame.newHow2play;
+        //* Mobile game *
+        game.mobile_game =
+          updateGame.newNobile_game == null
+            ? game.mobile_game
+            : updateGame.newMobile_game;
+        console.log(game);
+        game.save();
+        res.send(game);
+      });
     } catch (err) {
-      resp.send(err);
+      console.log(err);
     }
-  });
-});
-app.put("/api/games/update_rank/:id", async (req, res) => {
-  let id = req.params.id;
-  let updateGame = req.body;
-  try {
-    await games.findById(id, (err, game) => {
-      game.rank = updateGame.rank;
-      game.save();
-      res.send(game);
-    });
-  } catch (err) {
-    console.log(err);
   }
-});
+);
+app.delete(
+  "/api/envgame/conchuot2945@/2945/games/delete/:id",
+  async (req, res) => {
+    let _id = req.params.id;
+    games.findByIdAndDelete(_id, (err, txt) => {
+      try {
+        res.send("Xóa thành công _ID: " + _id);
+      } catch (err) {
+        resp.send(err);
+      }
+    });
+  }
+);
+app.put(
+  "/api/envgame/conchuot2945@/2945/games/update_rank/:id",
+  async (req, res) => {
+    let id = req.params.id;
+    let updateGame = req.body;
+    try {
+      await games.findById(id, (err, game) => {
+        game.rank = updateGame.rank;
+        game.save();
+        res.send(game);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
 //===============================================================================
 //================================USER============================================
 //===============================================================================
-app.post("/api/users/create", async (req, res) => {
+app.post("/api/envgame/conchuot2945@/2945/users/create", async (req, res) => {
   let usera = new users(req.body);
   console.log(usera);
   try {
@@ -125,7 +135,7 @@ app.post("/api/users/create", async (req, res) => {
     console.log(e);
   }
 });
-app.get("/api/users/read", async (req, res) => {
+app.get("/api/envgame/conchuot2945@/2945/users/read", async (req, res) => {
   users.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -133,31 +143,37 @@ app.get("/api/users/read", async (req, res) => {
     res.send(database);
   });
 });
-app.put("/api/users/update/:id", async (req, res) => {
-  let id = req.params.id;
-  let updateUser = req.body;
-  try {
-    await users.findById(id, (err, user) => {
-      user.played_games = updateUser.played_games;
-      user.time_gaming = updateUser.time_gaming;
-      user.save();
-      res.send(user);
-    });
-  } catch (err) {
-    console.log(err);
+app.put(
+  "/api/envgame/conchuot2945@/2945/users/update/:id",
+  async (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+    try {
+      await users.findById(id, (err, user) => {
+        user.played_games = updateUser.played_games;
+        user.time_gaming = updateUser.time_gaming;
+        user.save();
+        res.send(user);
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
-});
+);
 // ========================== Feedback===========================
-app.post("/api/feedbacks/create", async (req, res) => {
-  let feedbacka = new feedbacks(req.body);
-  try {
-    await feedbacka.save();
-    res.send(req.body);
-  } catch (e) {
-    console.log(e);
+app.post(
+  "/api/envgame/conchuot2945@/2945/feedbacks/create",
+  async (req, res) => {
+    let feedbacka = new feedbacks(req.body);
+    try {
+      await feedbacka.save();
+      res.send(req.body);
+    } catch (e) {
+      console.log(e);
+    }
   }
-});
-app.get("/api/feedbacks/read", async (req, res) => {
+);
+app.get("/api/envgame/conchuot2945@/2945/feedbacks/read", async (req, res) => {
   feedbacks.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -166,7 +182,7 @@ app.get("/api/feedbacks/read", async (req, res) => {
   });
 });
 // ========================== Feedback===========================
-app.get("/api/chats/read", async (req, res) => {
+app.get("/api/envgame/conchuot2945@/2945/chats/read", async (req, res) => {
   chats.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -174,7 +190,7 @@ app.get("/api/chats/read", async (req, res) => {
     res.send(database);
   });
 });
-app.post("/api/chats/create", async (req, res) => {
+app.post("/api/envgame/conchuot2945@/2945/chats/create", async (req, res) => {
   let chata = new chats(req.body);
   try {
     await chata.save();
