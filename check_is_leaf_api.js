7f293e96a2c6ba836a61168d586b3171163cb68a
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-09-07 14:37:05 +0700

    fix bug

--- check_is_leaf_api.js
+++ check_is_leaf_api.js
@@ -1,60 +1,60 @@
-const got = require("got"); // if you don't have "got" - install it with "npm install got"
-const apiKey = "acc_a94e63861293515";
-const apiSecret = "616653c74f9bdf3d2c6595aea6c3dffa";
-const axios = require("axios").default;
-module.exports = {
-  detection_imagga: function (UPLOAD_URL, IO) {
-    console.log("loading detection imagga... for:" + UPLOAD_URL);
-    const url =
-      "http://api.imagga.com/v2/tags?image_url=" +
-      encodeURIComponent(UPLOAD_URL);
+// const got = require("got"); // if you don't have "got" - install it with "npm install got"
+// const apiKey = "acc_a94e63861293515";
+// const apiSecret = "616653c74f9bdf3d2c6595aea6c3dffa";
+// const axios = require("axios").default;
+// module.exports = {
+//   detection_imagga: function (UPLOAD_URL, IO) {
+//     console.log("loading detection imagga... for:" + UPLOAD_URL);
+//     const url =
+//       "http://api.imagga.com/v2/tags?image_url=" +
+//       encodeURIComponent(UPLOAD_URL);
 
-    (async () => {
-      try {
-        const response = await got(url, {
-          username: apiKey,
-          password: apiSecret,
-        });
-        // console.log("load successfully!");
-        const AI_IN4 = {
-          detection: response.body,
-          mode: "imagga",
-        };
-        IO.emit("AI detect imagga", AI_IN4);
-      } catch (error) {
-        console.log(error.response.body);
-      }
-    })();
-  },
-  detetion_vision: function (UPLOAD_URL, IO) {
-    console.log("loading detection vision... for:" + UPLOAD_URL);
-    // dự phòng nếu imagga API kiểm tra là lá bị hỏng
-    var options = {
-      method: "POST",
-      url: "https://google-ai-vision.p.rapidapi.com/cloudVision/imageCopyrightInfringementDetection",
-      headers: {
-        "content-type": "application/json",
-        "x-rapidapi-host": "google-ai-vision.p.rapidapi.com",
-        "x-rapidapi-key": "eb97f0d321mshb47557d453f72dcp1c0748jsne543b4e7e6e3",
-      },
-      data: {
-        source: UPLOAD_URL,
-        sourceType: "url",
-      },
-    };
+//     (async () => {
+//       try {
+//         const response = await got(url, {
+//           username: apiKey,
+//           password: apiSecret,
+//         });
+//         // console.log("load successfully!");
+//         const AI_IN4 = {
+//           detection: response.body,
+//           mode: "imagga",
+//         };
+//         IO.emit("AI detect imagga", AI_IN4);
+//       } catch (error) {
+//         console.log(error.response.body);
+//       }
+//     })();
+//   },
+//   detetion_vision: function (UPLOAD_URL, IO) {
+//     console.log("loading detection vision... for:" + UPLOAD_URL);
+//     // dự phòng nếu imagga API kiểm tra là lá bị hỏng
+//     var options = {
+//       method: "POST",
+//       url: "https://google-ai-vision.p.rapidapi.com/cloudVision/imageCopyrightInfringementDetection",
+//       headers: {
+//         "content-type": "application/json",
+//         "x-rapidapi-host": "google-ai-vision.p.rapidapi.com",
+//         "x-rapidapi-key": "eb97f0d321mshb47557d453f72dcp1c0748jsne543b4e7e6e3",
+//       },
+//       data: {
+//         source: UPLOAD_URL,
+//         sourceType: "url",
+//       },
+//     };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-        const AI_IN4 = {
-          detection: response.data.webDetection.webEntities,
-          mode: "vision",
-        };
-        IO.emit("AI detect vision", AI_IN4);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  },
-};
+//     axios
+//       .request(options)
+//       .then(function (response) {
+//         console.log(response.data);
+//         const AI_IN4 = {
+//           detection: response.data.webDetection.webEntities,
+//           mode: "vision",
+//         };
+//         IO.emit("AI detect vision", AI_IN4);
+//       })
+//       .catch(function (error) {
+//         console.error(error);
+//       });
+//   },
+// };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const db = require("./model");
-const check_is_leaf = require("./check_is_leaf_api");
+// const check_is_leaf = require("./check_is_leaf_api");
 const games = db.games;
 const users = db.users;
 const feedbacks = db.feedbacks;
@@ -10,6 +10,10 @@ const chats = db.chats;
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -22,10 +26,7 @@ db.mongoose
     console.log(e);
     process.exit();
   });
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-app.post("/api/envgame/conchuot2945@/2945/games/create", async (req, res) => {
+app.post("/api/envgame/games/create", async (req, res) => {
   let gamea = new games(req.body);
   try {
     await gamea.save();
@@ -34,7 +35,8 @@ app.post("/api/envgame/conchuot2945@/2945/games/create", async (req, res) => {
     console.log(e);
   }
 });
-app.get("/api/envgame/conchuot2945@/2945/games/read", async (req, res) => {
+
+app.get("/api/envgame/games/read", async (req, res) => {
   games.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -42,81 +44,71 @@ app.get("/api/envgame/conchuot2945@/2945/games/read", async (req, res) => {
     res.send(database);
   });
 });
-app.put(
-  "/api/envgame/conchuot2945@/2945/games/update/:id",
-  async (req, res) => {
-    let id = req.params.id;
-    let updateGame = req.body;
-    try {
-      await games.findById(id, (err, game) => {
-        //* title *
-        game.title =
-          updateGame.newTitle == "" ? game.title : updateGame.newTitle;
-        //* description *
-        game.description =
-          updateGame.newDescription == ""
-            ? game.description
-            : updateGame.newDescription;
-        //* url *
-        game.url = updateGame.newUrl == "" ? game.url : updateGame.newUrl;
-        //* iframe *
-        game.iframe =
-          updateGame.newIframe == "" ? game.iframe : updateGame.newIframe;
-        //* love game *
-        game.love_game =
-          updateGame.newLove_game == null
-            ? game.love_game
-            : updateGame.newLove_game;
-        //* how2play *
-        game.how2play =
-          updateGame.newHow2play == "" ? game.how2play : updateGame.newHow2play;
-        //* Mobile game *
-        game.mobile_game =
-          updateGame.newNobile_game == null
-            ? game.mobile_game
-            : updateGame.newMobile_game;
-        console.log(game);
-        game.save();
-        res.send(game);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-);
-app.delete(
-  "/api/envgame/conchuot2945@/2945/games/delete/:id",
-  async (req, res) => {
-    let _id = req.params.id;
-    games.findByIdAndDelete(_id, (err, txt) => {
-      try {
-        res.send("Xóa thành công _ID: " + _id);
-      } catch (err) {
-        resp.send(err);
-      }
+app.put("/api/envgame/games/update/:id", async (req, res) => {
+  let id = req.params.id;
+  let updateGame = req.body;
+  try {
+    await games.findById(id, (err, game) => {
+      //* title *
+      game.title = updateGame.newTitle == "" ? game.title : updateGame.newTitle;
+      //* description *
+      game.description =
+        updateGame.newDescription == ""
+          ? game.description
+          : updateGame.newDescription;
+      //* url *
+      game.url = updateGame.newUrl == "" ? game.url : updateGame.newUrl;
+      //* iframe *
+      game.iframe =
+        updateGame.newIframe == "" ? game.iframe : updateGame.newIframe;
+      //* love game *
+      game.love_game =
+        updateGame.newLove_game == null
+          ? game.love_game
+          : updateGame.newLove_game;
+      //* how2play *
+      game.how2play =
+        updateGame.newHow2play == "" ? game.how2play : updateGame.newHow2play;
+      //* Mobile game *
+      game.mobile_game =
+        updateGame.newNobile_game == null
+          ? game.mobile_game
+          : updateGame.newMobile_game;
+      console.log(game);
+      game.save();
+      res.send(game);
     });
+  } catch (err) {
+    console.log(err);
   }
-);
-app.put(
-  "/api/envgame/conchuot2945@/2945/games/update_rank/:id",
-  async (req, res) => {
-    let id = req.params.id;
-    let updateGame = req.body;
+});
+app.delete("/api/envgame/games/delete/:id", async (req, res) => {
+  let _id = req.params.id;
+  games.findByIdAndDelete(_id, (err, txt) => {
     try {
-      await games.findById(id, (err, game) => {
-        game.rank = updateGame.rank;
-        game.save();
-        res.send(game);
-      });
+      res.send("Xóa thành công _ID: " + _id);
     } catch (err) {
-      console.log(err);
+      resp.send(err);
     }
+  });
+});
+app.put("/api/envgame/games/update_rank/:id", async (req, res) => {
+  let id = req.params.id;
+  let updateGame = req.body;
+  try {
+    await games.findById(id, (err, game) => {
+      game.rank = updateGame.rank;
+      game.save();
+      res.send(game);
+    });
+  } catch (err) {
+    console.log(err);
   }
-);
-//===============================================================================
+});
+//==============================================================================
 //================================USER============================================
 //===============================================================================
-app.post("/api/envgame/conchuot2945@/2945/users/create", async (req, res) => {
+app.post("/api/envgame/users/create", async (req, res) => {
   let usera = new users(req.body);
   console.log(usera);
   try {
@@ -126,7 +118,7 @@ app.post("/api/envgame/conchuot2945@/2945/users/create", async (req, res) => {
     console.log(e);
   }
 });
-app.get("/api/envgame/conchuot2945@/2945/users/read", async (req, res) => {
+app.get("/api/envgame/users/read", async (req, res) => {
   users.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -134,37 +126,31 @@ app.get("/api/envgame/conchuot2945@/2945/users/read", async (req, res) => {
     res.send(database);
   });
 });
-app.put(
-  "/api/envgame/conchuot2945@/2945/users/update/:id",
-  async (req, res) => {
-    let id = req.params.id;
-    let updateUser = req.body;
-    try {
-      await users.findById(id, (err, user) => {
-        user.played_games = updateUser.played_games;
-        user.time_gaming = updateUser.time_gaming;
-        user.save();
-        res.send(user);
-      });
-    } catch (err) {
-      console.log(err);
-    }
+app.put("/api/envgame/users/update/:id", async (req, res) => {
+  let id = req.params.id;
+  let updateUser = req.body;
+  try {
+    await users.findById(id, (err, user) => {
+      user.played_games = updateUser.played_games;
+      user.time_gaming = updateUser.time_gaming;
+      user.save();
+      res.send(user);
+    });
+  } catch (err) {
+    console.log(err);
   }
-);
+});
 // ========================== Feedback===========================
-app.post(
-  "/api/envgame/conchuot2945@/2945/feedbacks/create",
-  async (req, res) => {
-    let feedbacka = new feedbacks(req.body);
-    try {
-      await feedbacka.save();
-      res.send(req.body);
-    } catch (e) {
-      console.log(e);
-    }
+app.post("/api/envgame/feedbacks/create", async (req, res) => {
+  let feedbacka = new feedbacks(req.body);
+  try {
+    await feedbacka.save();
+    res.send(req.body);
+  } catch (e) {
+    console.log(e);
   }
-);
-app.get("/api/envgame/conchuot2945@/2945/feedbacks/read", async (req, res) => {
+});
+app.get("/api/envgame/feedbacks/read", async (req, res) => {
   feedbacks.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -173,7 +159,7 @@ app.get("/api/envgame/conchuot2945@/2945/feedbacks/read", async (req, res) => {
   });
 });
 // ========================== Feedback===========================
-app.get("/api/envgame/conchuot2945@/2945/chats/read", async (req, res) => {
+app.get("/api/envgame/chats/read", async (req, res) => {
   chats.find({}, (err, database) => {
     if (err) {
       res.send(err);
@@ -181,7 +167,7 @@ app.get("/api/envgame/conchuot2945@/2945/chats/read", async (req, res) => {
     res.send(database);
   });
 });
-app.post("/api/envgame/conchuot2945@/2945/chats/create", async (req, res) => {
+app.post("/api/envgame/chats/create", async (req, res) => {
   let chata = new chats(req.body);
   try {
     await chata.save();
@@ -190,18 +176,16 @@ app.post("/api/envgame/conchuot2945@/2945/chats/create", async (req, res) => {
     console.log(e);
   }
 });
-
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 // var server = require("http").Server(app);
 // const options = {
 //   cors: {
 //     origin: "*",
 //   },
 // };
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
 // const io = require("socket.io")(server, options);
 
 // io.on("connection", function (socket) {
