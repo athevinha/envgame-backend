4277948eab6a70bc6a2fb75aea5556869db3c095
Envgame_Github <nguyenthevinh1002@gmail.com>
2021-09-07 10:15:28 +0700

    fix bug

--- server.js
+++ server.js
@@ -202,7 +202,8 @@ const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, { cors: true });
+
 io.on("connection", function (socket) {
   console.log("+1 connections !!!");
   socket.on("disconnect", function () {
