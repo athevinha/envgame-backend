86bf4ad3ee8894a036b2d9a0f6fc279ea1b62fe0
athevinha <nguyenthevinh1002@gmail.com>
2022-03-14 22:05:30 +0700

    add cloudinary

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const cors = require("cors");
 const app = express();
 const db = require("./model");
 const check_is_leaf = require("./check_is_leaf_api");
+const cloudinary = require("cloudinary").v2;
 const games = db.games;
 const users = db.users;
 const feedbacks = db.feedbacks;
 const chats = db.chats;
 const got = require("got");
+cloudinary.config({
+  cloud_name: "envgame",
+  api_key: "336187849671285",
+  api_secret: "w6UXeVVwn0-uYH3zA-YH0q4NYiY",
+  secure: true,
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 var corsOptions = {
   origin: "http://localhost:8081",
@@ -198,6 +206,15 @@ app.post("/api/envgame/ai/detect", async (req, res) => {
     console.error(error);
   }
 });
+app.post("/upload", (req, res) => {
+  let base64 = req.body.base64;
+  cloudinary.uploader.upload(base64, function (error, result) {
+    console.log(result, error);
+    res.send({ result, error });
+  });
+  //   res.send("vdf");
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
